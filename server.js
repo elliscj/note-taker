134690186d58b8c50bb2040c9fc8e94abddd93a1
Cam Ellis <elliscj@colorado.edu>
2021-12-07 18:50:08 -0700

    working get post routs

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "./public/index.html"))
+);
+
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "./public/notes.html"))
+);
+
+app.get("/api/notes", (req, res) => {
+  fs.readFile("./db/db.json", (err, data) => {
+    console.log(err);
+    console.log(JSON.parse(data));
+    res.json(JSON.parse(data));
+  });
+});
+
+app.post("/api/notes", (req, res) => {
+  // read db
+  fs.readFile("./db/db.json", (err, data) => {
+    console.log(err);
+    console.log(JSON.parse(data));
+    // parse data
+    let parsedNotes = JSON.parse(data);
+    // res.json(JSON.parse(data));
+    // add new note to data
+    parsedNotes.push(req.body);
+    // save stringify db
+    fs.writeFile("./db/db.json", JSON.stringify(parsedNotes), (err) => {
+      console.log(err);
+      res.json(req.body);
+    });
+  });
+  // send db json
+});
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
